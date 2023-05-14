8ceff5b48bda6ddae299c091a65772c5195c9089
Vladislav <vlad.melnichenko96@gmail.com>
2023-05-14 03:30:52 +0300

    [front] chat updates at realtime

--- front/src/app/room/chat/chat.component.ts
+++ front/src/app/room/chat/chat.component.ts
@@ -1,25 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { WebsocketService } from '../../services/websocket.service';
 import { ChatMessage, ChatService } from '../../services/chat.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SocialUser } from '@abacritt/angularx-social-login';
 import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   @Input() roomID = ''
   @Input() user!: SocialUser
+
+  @ViewChildren('messagesElem') messagesElem!: QueryList<any>;
+  @ViewChild('content') content!: ElementRef;
 
   chatInput = new FormControl('', [Validators.required, Validators.min(1)])
   messages: ChatMessage[] = []
   chatForm = this._formBuilder.group({
     msg: this.chatInput
   });
+  // @ts-ignore
+  ws: WebSocketSubject<any>
 
   constructor(private authService: AuthService,
               private snackBar: MatSnackBar,
@@ -40,8 +46,25 @@ export class ChatComponent implements OnInit {
     })
   }
 
+  ngAfterViewInit() {
+    this.scrollToBottom();
+    this.messagesElem.changes.subscribe(this.scrollToBottom);
+  }
+
+  scrollToBottom = () => {
+    try {
+      this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
+
   conn() {
-    this.websocketService.conn(String(this.roomID))
+    this.ws = this.websocketService.conn(String(this.roomID))
+
+    this.ws.subscribe({
+      next: v => {
+        this.messages.push(v)
+      }
+    })
   }
 
   sendMsg() {
@@ -50,4 +73,11 @@ export class ChatComponent implements OnInit {
       this.chatInput.setValue("")
     }
   }
+
+  ngOnDestroy(): void {
+    this.ws.complete()
+  }
+
+
+
 }
--- front/src/app/services/websocket.service.ts
+++ front/src/app/services/websocket.service.ts
@@ -2,44 +2,31 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
 
-  ws: WebSocket = {} as WebSocket
+  // @ts-ignore
+  subject: WebSocketSubject<any>
 
   constructor(private authService: AuthService) {
   }
 
-  conn(roomID: string) {
-    this.ws = new WebSocket(`ws://localhost:${environment.port}/api/rooms/${roomID}/ws`);
+  conn(roomID: string) : WebSocketSubject<any> {
+    this.subject = webSocket(`ws://localhost:${environment.port}/api/rooms/${roomID}/ws`);
 
-    this.ws.onopen = () => {
-      this.ws.onmessage = (event) => {
-        if (event.data == "ping") {
-          this.ws.send("pong")
-          return
-        }
-      }
-
-      JSON.stringify({
-        email: this.authService.user?.email,
-        content: "Here's some text that the server is urgently awaiting!"
-      })
-
-      // this.ws.send(this.makeWSMessage("Here's some text that the server is urgently awaiting!"));
-    };
+    return this.subject
   }
+
+
 
   sendMsg(msg: string) {
-    this.ws.send(this.makeWSMessage(msg));
-  }
-
-  makeWSMessage(content: string): string {
-    return JSON.stringify({
+    this.subject.next({
       email: this.authService.user?.email ?? '',
-      content: content
+      content: msg
     })
   }
 }
